ac55857de203c1e5ff627574885641539579324b
hmh88-jpg <hmh885914@gmail.com>
2025-12-20 00:02:30 +0300

    Update add.js

--- frontend/js/add.js
+++ frontend/js/add.js
@@ -1,24 +1,31 @@
 const API = "https://perfume-world.onrender.com/api/perfumes";
 
-document.getElementById("addForm").addEventListener("submit", e => {
+document.getElementById("addForm").addEventListener("submit", function (e) {
   e.preventDefault();
 
-  const data = {
-    name: name.value,
-    price: price.value,
-    image: image.value,
-    rating: rating.value,
-    description: description.value
+  const perfume = {
+    name: document.getElementById("name").value,
+    price: document.getElementById("price").value,
+    image: document.getElementById("image").value,
+    rating: document.getElementById("rating").value,
+    description: document.getElementById("description").value
   };
 
   fetch(API, {
     method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(data)
-  }).then(() => {
-    alert("تمت إضافة العطر بنجاح");
-    window.location.href = "list.html";
-  });
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(perfume)
+  })
+    .then(res => res.json())
+    .then(() => {
+      alert("تمت إضافة العطر بنجاح 🌸");
+      window.location.href = "list.html";
+    })
+    .catch(err => console.log(err));
 });
+
+
 
 
